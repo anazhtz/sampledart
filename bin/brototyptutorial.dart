4271cd4d896e3da67cdde3f4f73d22bc54f711f9
anazhtz <anazbinashraf2@gmail.com>
2023-09-06 00:15:49 +0530

    commit

--- bin/brototyptutorial.dart
+++ bin/brototyptutorial.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 
 void main() {
   //if statment
@@ -15,19 +14,47 @@ void main() {
 
   //loop statment
   //for (var i = 0; i <=10; i++) {
-   // print(i);
- // }
+  // print(i);
+  // }
 
   //list
- List<int> age=[10,12,13,14,15];
- Set<int>age1={10,12,13,14,15,10,12,13,14,15};  //set same as list but we can onl;y apply here Single values means no duplicate value not workinbg
- print(age);
- print(age1);
-var maps={  // map case no repeated keys measn duplicat keys are not allowed here
-   "name": "Anaz",
-   "Age" : 24,
- };
- print(maps['name']);
- print(maps['Age']);
+  //List<int> age=[10,12,13,14,15];
+// Set<int>age1={10,12,13,14,15,10,12,13,14,15};  //set same as list but we can onl;y apply here Single values means no duplicate value not workinbg
+  //print(age);
+  //print(age1);
+//var maps={  // map case no repeated keys measn duplicat keys are not allowed here
+  // "name": "Anaz",
+  // "Age" : 24,
+//};
+  //print(maps['name']);
+  //print(maps['Age']);
+
+  // properties
+  String classname= "Flutter";
+  print(classname.isEmpty);
+  print(classname.length);
+ print(classname.toUpperCase());  // small leter ullathine upper caselek mattaan
+  int age= 28;
+  print(age);
+  print(age.isEven); //even number check cheyyan
+ print(age.isOdd); //odd nuymber chekk cheyyan
+ print(age.isNegative);  //negative value aano check cheyyan
+  double age1= 24.72;
+  print(age1);
+  print(age1.round()); // point shesham ulla value round off cheyth varum
+  List<String> detail= ["Anaz" , "NEhla", "Rashad","Akash"];
+  print(detail);
+  print(detail.isEmpty);  //list value undo elleyo check cheyyaan
+  print(detail.isNotEmpty);
+  detail.add("Sayand");  //new value add cheyyan
+  print(detail);
+  detail.insert(2, "Rahul");  //insert namukk value ead postion aano vnde aa position mark cheyyan then value add cheyya
+  print(detail);
+
+
+
 
 }
+
+
+
--- bin/mapINdart.dart
+++ bin/mapINdart.dart
@@ -12,6 +12,17 @@ void main(){
   map.forEach((key, value) {
     print("$key = $value" );
   });
+ print("the key are ${map1.keys}");  //keys value mathram print cheyyaan
+ print("the key are ${map1.values}"); // value mathram print cheyyan
+ map.addAll({"Email" : "anazbinashraf2@gmail.com"});  //new value ulla mapil add cheyyam
+  map.forEach((key, value) {
+    print("$key = $value");
+  });
+Map<String,dynamic> value={"Gender" : "Male", "adhar no" : 25541};
+ //value.clear();  //map ulla ellaa value delet cheyyan
+ print(value);
+ value.remove("adhar no");
+ print(value);
 
 
 }
