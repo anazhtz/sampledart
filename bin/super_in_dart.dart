5f4925fc3c42cf78bc8f4c6fd4e183456f9696b3
anazhtz <anazbinashraf2@gmail.com>
2023-09-12 13:59:56 +0530

    commit

--- bin/super_in_dart.dart
+++ bin/super_in_dart.dart
@@ -1,15 +1,27 @@
-class Car{
-  int speed=240;
+class Car {
+  int speed = 240;
+  void display() {
+    print("inside a parent class function");
+  }
 }
-class Bike extends Car{
+
+class Bike extends Car {
   @override
-  int speed=180;
-  void show(){
+  int speed = 180;
+  void show() {
     print("the speed of Car : ${super.speed}");
     print("the speed of bike : $speed");
   }
+
+  @override
+  void display() {
+    print("child function");
+    super.display();
+  }
 }
-void main(){
-  Bike obj=Bike();
+
+void main() {
+  Bike obj = Bike();
   obj.show();
+  obj.display();
 }
