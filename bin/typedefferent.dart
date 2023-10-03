826d643dbfb20c224a9bbf3f228e313e27e4af34
anazhtz <anazbinashraf@gmail.com>
2023-10-03 14:02:06 +0530

    commit

--- bin/typedefferent.dart
+++ bin/typedefferent.dart
@@ -3,7 +3,16 @@ typedef maths(int a,int b);
 
 void show(int a,int b){
   int c=a+b;
+  print(c);
 }
 void display(int c,int d){
   int f=c+d;
+  print(f);
+}
+void main(){
+  maths obj=show;
+ obj(50,10);
+  obj=display;
+  obj(20,10);
+
 }
